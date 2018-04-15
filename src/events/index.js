62bedfda5d9d4e9c55577c2f5ec96eb5103b1cfb
Hugo Larrousse <hugo.larrousse@chance.co>
2018-04-14 23:36:14 -0300

    add events route

---
+++ src/events/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { httpRequest } = require('../utils/httpRequest');
+
+const router = express.Router();
+
+const defaultModel = {
+  method: 'GET',
+  uri: 'https://api.github.com/events',
+  headers: {
+    Authorization: process.env.GITHUB_TOKEN,
+    'User-Agent': 'Test App',
+  },
+  body: null,
+  json: true,
+};
+
+
+router.get('/', async (req, res) => {
+  httpRequest(defaultModel, (result) => {
+    return result ? res.status(200).json(result) : res.status(400).send(null);
+  });
+});
+
+
+module.exports = router;
