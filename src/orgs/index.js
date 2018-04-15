28f2c9af5d983d2109000fbfa2da9d0a8a630a5b
Hugo Larrousse <hugo.larrousse@chance.co>
2018-04-14 23:36:21 -0300

    add orgs route

---
+++ src/orgs/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { httpRequest } = require('../utils/httpRequest');
+
+const router = express.Router();
+
+const defaultModel = {
+  method: 'GET',
+  uri: 'https://api.github.com/orgs',
+  headers: {
+    Authorization: process.env.GITHUB_TOKEN || null,
+    'User-Agent': 'Test App',
+  },
+  body: null,
+  json: true,
+};
+
+router.get('/', async (req, res) => {
+  res.status(200).json({
+    success: false, message: '/orgs doesn\'t exist, instead you should use /organizations or /orgs/<login of an organization>',
+  });
+});
+
+
+router.get('/:orgName', async (req, res) => {
+  httpRequest({ ...defaultModel, path: req.params.orgName }, (result) => {
+    return result ? res.status(200).json(result) : res.status(400).send(null);
+  });
+});
+
+
+module.exports = router;
