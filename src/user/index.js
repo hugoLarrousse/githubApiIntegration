832e83c88356dea0ec0fd68462dc4cab2448791f
Hugo Larrousse <hugo.larrousse@chance.co>
2018-04-16 17:54:22 -0300

    remove useless comments

--- src/user/index.js
+++ src/user/index.js
@@ -14,20 +14,6 @@ const defaultModel = {
   json: true,
 };
 
-// const optionsPatch = {
-//   method: 'PATCH',
-//   uri: 'https://api.github.com/user',
-//   headers: {
-//     Authorization: process.env.GITHUB_TOKEN,
-//     'User-Agent': 'Test App',
-//   },
-//   body: {
-//     name: 'Hugo Larrousse',
-//   },
-//   json: true,
-// }
-
-
 router.get('/', async (req, res) => {
   httpRequest(defaultModel, (result) => {
     return result ? res.status(200).json(result) : res.status(400).send(null);
